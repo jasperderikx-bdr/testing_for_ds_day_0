e02768ec7e883bf82521327bd0d5f7152f03c590
Your Name <jasper.derikx@vantage-ai.com>
2022-03-17 13:29:51 +0100

    add 2 exercises

--- tests/test_exercises.py
+++ tests/test_exercises.py
@@ -1,2 +1,18 @@
+import pytest
+
+from src.math_functions import increment
+
+
 def test_exercise_0() -> None:
-    assert False
+    # Make this test pass, by changing the value of expected_result.
+    x = 10
+    result = increment(x)
+    expected_result = 5
+    assert result == expected_result
+
+
+def test_exercise_1() -> None:
+    # To avoid unexpected behaviour, we want the function increment to raise an error when the input is a boolean.
+    # The test is written correctly. Make it pass, by changing the implementation of function increment.
+    with pytest.raises(expected_exception=TypeError):
+        increment(True)
