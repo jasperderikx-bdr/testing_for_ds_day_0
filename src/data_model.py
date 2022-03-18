57d57745ad57ebb8e9979e55187e50a1d2735958
Your Name <jasper.derikx@vantage-ai.com>
2022-03-18 13:29:47 +0100

    Finish exercises

--- src/data_model.py
+++
@@ -1,7 +0,0 @@
-class Employee:
-    def __init__(self, name: str, age: int) -> None:
-        self.name = name
-        self.age = age
-
-    def give_birthday(self) -> None:
-        self.age = self.age + 1
--- tests/test_exercises.py
+++ tests/test_exercises.py
@@ -3,17 +3,27 @@ import pytest
 from src.math_functions import increment
 
 
-def test_exercise_0() -> None:
-    # Make this test pass, by changing the value of expected_result.
+# -- Exercise 1 --
+# Make this test pass.
+def test_increment_example() -> None:
     x = 10
     result = increment(x)
     expected_result = 5
     assert result == expected_result
 
 
-def test_exercise_1() -> None:
-    # To avoid unexpected behaviour, we want the function increment to raise an error when the input is a boolean.
-    # The test is written correctly. Make it pass, by changing the implementation of function increment.
-    increment(True)
+# -- Exercise 2 --
+# To avoid unexpected behaviour, we want increment() to raise an error when the input is a boolean.
+# The test is written correctly. Make it pass, by expanding the implementation of increment().
+def test_type_error_on_boolean() -> None:
     with pytest.raises(expected_exception=TypeError):
         increment(True)
+
+
+# -- Exercise 3 --
+# Similarly, we would like to test if a TypeError is raised when increment() is called with a string. Start by
+# implementing this test.
+# The problem that arises is that our type checker MyPy, won't accept this code. You can see this when you try to commit
+# the code. Finish by making sure that MyPy ignores this (on purpose) iltyped line of code.
+def test_type_error_on_string() -> None:
+    pass
